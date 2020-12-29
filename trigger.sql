6dc864db671a6d2a8b4b98450464fb377b4ffb7d
NQLong <nqlong0709@gmail.com>
2020-12-29 21:06:33 +0700

    triiger

--- trigger.sql
+++ trigger.sql
@@ -15,4 +15,24 @@ BEGIN
     END IF;
 END$$
 
-DELIMITER;
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE TRIGGER after_BienBanGui_insert
+AFTER INSERT
+ON BienBanGui FOR EACH ROW
+BEGIN
+    IF new.YeuCauLayHang <> null THEN
+        If select count(*) from YeuCauLayHang <> 0 Then
+            update YeuCauLayHang
+            Set TrangThai = 'DaXuLy'
+            Where ID = old.YeuCauID;
+        End IF;
+    END IF;
+END$$
+
+DELIMITER ;
+
+
+
