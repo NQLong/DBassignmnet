14df422379ef4eaadba2d93b9ece41d75d0cb45d
NQLong <nqlong0709@gmail.com>
2020-12-29 21:52:55 +0700

    fix ddl

--- insert.sql
+++ insert.sql
@@ -1,1 +1,3 @@
-call INSERT_TAIXE()
+call INSERT_TAIXE('12345678','2012-12-12','123455', '12345', '6789');
+select * from TaiXe join NhanVien where TaiXe.CMND = NhanVien.CMND;
+delete from TaiXe where CMND = '12345678';
